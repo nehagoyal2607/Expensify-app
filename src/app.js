ecbcef01092a306505fcb07fe0903750c31ab73d
Neha <goyalneha2607@gmail.com>
2021-01-01 22:37:02 +0530

    Private route added

--- src/app.js
+++ src/app.js
@@ -2,16 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 
-import AppRouter from './routers/AppRouter.js';
+import AppRouter , {history} from './routers/AppRouter.js';
 import configureStore from './store/configureStore';
 import {startSetExpenses} from './actions/expenses';
-import {setTextFilter} from './actions/filters';
+import {login , logout} from './actions/auth';
 import getVisibleExpenses from './selectors/expenses';
 
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
-import './firebase/firebase';
+import {firebase} from './firebase/firebase';
+import { render } from 'enzyme';
 
 
 const store = configureStore();
@@ -33,11 +34,31 @@ const jsx  = (
     <Provider store={store}>
         <AppRouter/>
     </Provider>
-)
-    
+);
+let hasRendered = false;
+const renderApp = () => {
+    if(!hasRendered){
+        ReactDOM.render(jsx, document.getElementById('app'));
+        hasRendered = true;
+    }
+}
+
 ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
 
-store.dispatch(startSetExpenses()).then(()=>{
-    ReactDOM.render(jsx, document.getElementById('app'));
-})
-
+firebase.auth().onAuthStateChanged((user)=>{
+    if(user){
+        // console.log("Log in");
+        store.dispatch(login(user.uid));
+        store.dispatch(startSetExpenses()).then(()=>{
+            renderApp();
+            if(history.location.pathname === '/'){
+                history.push('/dashboard');
+            }
+        })
+    }else{
+        // console.log("Log out");
+        store.dispatch(logout());
+        renderApp();
+        history.push('/');
+    }
+});
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -13,8 +13,11 @@ const config = {
 firebase.initializeApp(config);
 
 const database = firebase.database();
-
-export {firebase, database as default};
+const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
+googleAuthProvider.setCustomParameters({
+  prompt: 'select_account'
+});
+export {firebase, googleAuthProvider, database as default};
 
 
 
